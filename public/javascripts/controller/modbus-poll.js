605491cdb15f2c211b7cd153635a6643259c5a7b
Eugen Kondratiev <kondratiev.es@gmail.com>
2023-11-19 14:50:11 +0200

    mode and path to .env

--- public/javascripts/controller/modbus-poll.js
+++ public/javascripts/controller/modbus-poll.js
@@ -73,25 +73,25 @@ async function mainModbusPoll(server) {
 
 
     const getDateTimeStringCurrent = require('../utils/get-last-day').getDateTimeStringCurrent
+    if (process.env.MODE !== "dev") {
+        try {
+            const retBLR4 = await connectPLC(client4, c.BLR4)
+            console.log("retBLR4 - ", retBLR4);
 
-    try {
-        // const retBLR4 = await connectPLC(client4, c.BLR4)
-        // console.log("retBLR4 - ", retBLR4);
+        } catch (error) {
+            logIt("connect PLC4 error" + error.message)
+        }
 
-    } catch (error) {
-        logIt("connect PLC4 error" + error.message)
+        try {
+
+            const retT5 = await connectPLC(client5, c.T5)
+            console.log("retT5 - ", retT5);
+        } catch (error) {
+            logIt("connect PLC5 error" + error.message)
+
+        }
+
     }
-
-    try {
-
-        // const retT5 = await connectPLC(client5, c.T5)
-        // console.log("retT5 - ", retT5);
-    } catch (error) {
-        logIt("connect PLC5 error" + error.message)
-
-    }
-
-
     maindata.blr4.data = [...Array(c.M340_VARIABLES_QUANTITY[c.BLR4] + c.M340_VARIABLES_QUANTITY[c.BLR4_2])];
     maindata.t5.data = [...Array(c.M340_VARIABLES_QUANTITY[c.T5])];
     console.log("c.M340_VARIABLES_QUANTITY[c.BLR4]+c.M340_VARIABLES_QUANTITY[c.BLR4_2]  ", c.M340_VARIABLES_QUANTITY[c.BLR4], c.M340_VARIABLES_QUANTITY[c.BLR4_2]);
@@ -103,44 +103,45 @@ async function mainModbusPoll(server) {
 
 
     let handler = setInterval(async function () {
+        if (process.env.MODE !== "dev") {
 
-        // try {
-        //     const _floats = await pollPlc(client4, c.CURRENT_START_MW[c.BLR4], 2 * (c.M340_VARIABLES_QUANTITY[c.BLR4] + c.M340_VARIABLES_QUANTITY[c.BLR4_2]))
-        //     _floats.forEach((fl, i) => {
-        //         maindata.blr4.data[i] = fl
-        //     });
+            try {
+                const _floats = await pollPlc(client4, c.CURRENT_START_MW[c.BLR4], 2 * (c.M340_VARIABLES_QUANTITY[c.BLR4] + c.M340_VARIABLES_QUANTITY[c.BLR4_2]))
+                _floats.forEach((fl, i) => {
+                    maindata.blr4.data[i] = fl
+                });
 
-        //     maindata.blr4.timestamp = getDateTimeStringCurrent((new Date()).toISOString())
-        // } catch (error) {
-        //     console.log(" clients[c.BLR4]  readHoldingRegisters ERROR", error);
-        //     maindata.blr4.data = maindata.blr4.data.map(i => null);
-        // }
+                maindata.blr4.timestamp = getDateTimeStringCurrent((new Date()).toISOString())
+            } catch (error) {
+                console.log(" clients[c.BLR4]  readHoldingRegisters ERROR", error);
+                maindata.blr4.data = maindata.blr4.data.map(i => null);
+            }
 
-        // try {
-        //     const _floats = await pollPlc(client5, c.CURRENT_START_MW[c.T5], 2 * c.M340_VARIABLES_QUANTITY[c.T5])
-        //     _floats.forEach((fl, i) => {
-        //         maindata.t5.data[i] = fl
-        //     });
+            try {
+                const _floats = await pollPlc(client5, c.CURRENT_START_MW[c.T5], 2 * c.M340_VARIABLES_QUANTITY[c.T5])
+                _floats.forEach((fl, i) => {
+                    maindata.t5.data[i] = fl
+                });
 
-        //     maindata.t5.timestamp = getDateTimeStringCurrent((new Date()).toISOString())
-        // } catch (error) {
-        //     console.log(" clients[c.T5]  readHoldingRegisters ERROR", error);
-        //     maindata.t5.data = maindata.t5.data.map(i => null);
-        // }
+                maindata.t5.timestamp = getDateTimeStringCurrent((new Date()).toISOString())
+            } catch (error) {
+                console.log(" clients[c.T5]  readHoldingRegisters ERROR", error);
+                maindata.t5.data = maindata.t5.data.map(i => null);
+            }
 
 
-        // try {
-        //     const _floats = await pollPlc(client5, c.CURRENT_START_MW[c.T5] + 2 * c.M340_VARIABLES_QUANTITY[c.T5], 2 * c.M340_VARIABLES_QUANTITY[c.T5_2])
-        //     _floats.forEach((fl, i) => {
-        //         maindata.t5.data[i + c.M340_VARIABLES_QUANTITY[c.T5]] = fl
-        //     });
+            try {
+                const _floats = await pollPlc(client5, c.CURRENT_START_MW[c.T5] + 2 * c.M340_VARIABLES_QUANTITY[c.T5], 2 * c.M340_VARIABLES_QUANTITY[c.T5_2])
+                _floats.forEach((fl, i) => {
+                    maindata.t5.data[i + c.M340_VARIABLES_QUANTITY[c.T5]] = fl
+                });
 
-        //     maindata.t5.timestamp = getDateTimeStringCurrent((new Date()).toISOString())
-        // } catch (error) {
-        //     console.log(" clients[c.T5_2]  readHoldingRegisters ERROR", error);
-        //     maindata.t5.data = maindata.t5.data.map(i => null);
-        // }
-
+                maindata.t5.timestamp = getDateTimeStringCurrent((new Date()).toISOString())
+            } catch (error) {
+                console.log(" clients[c.T5_2]  readHoldingRegisters ERROR", error);
+                maindata.t5.data = maindata.t5.data.map(i => null);
+            }
+        }
         io.sockets.emit('broadcast', JSON.stringify(global.maindata));
         // console.log("maindatat- ", maindata);
     }, c.DATA_COLLECT_PERIOD);
--- routes/abs-routes.js
+++ routes/abs-routes.js
@@ -1,4 +1,5 @@
-const views = "d:\\JS\\_eco\\eco4-express\\views";
+// const views = "d:\\JS\\_eco\\eco4-express\\views";
+const views = process.env.VIEWS
 
 module.exports = {
     views: views
